1240b538bed08ce47f37a2630e2d5cd4747b1476
Vokuc <ukpaival@gmail.com>
2022-04-08 16:17:09 -0700

    lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: in has to return (0)
+ */
+int main(void)
+{
+	int a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+
+	return (0);
+}
