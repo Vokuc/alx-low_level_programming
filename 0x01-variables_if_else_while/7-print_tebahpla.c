75701f9825b9e2301e8e4961bd8cd392126100e3
Vokuc <ukpaival@gmail.com>
2022-04-10 07:59:17 -0700

    reversed alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0 always
+ */
+int main(void)
+{
+	int a;
+
+	int b = a/2;
+
+	for (let a = 'a'; a <= 'z'; a++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+
+	return (0);
+}
