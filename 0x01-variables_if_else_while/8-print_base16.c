624a31abf730ec44018e0d1e9cca7d718f040b18
Vokuc <ukpaival@gmail.com>
2022-04-10 12:12:39 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0 for int function
+ */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a <= 9; a++)
+		putchar(a);
+
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+
+	putchar('\n');
+	
+	return 0;
+}
