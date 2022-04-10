3a128f746a19e95661af563d3488fdd67c3fe72f
Vokuc <ukpaival@gmail.com>
2022-04-10 06:09:37 -0700

    all single digit base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a);
+	}
+	
+	putchar("\n");
+
+	return (0);
+}
