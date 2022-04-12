2dccfbd27c646b787bed349a85588931e97a0695
Vokuc <ukpaival@gmail.com>
2022-04-12 11:50:34 -0700

    is lower check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - functions entry point
+ *
+ * Return: return 0 for int function
+ */
+int _islower(int c)
+{
+	int a = 1;
+	int b = 0;
+
+	if (c == islower(c))
+	{
+		_putchar(a);
+	}
+	else
+	{
+		_puchar(b);
+	}
+	_putchar('\n');
+	return (0);
+}
