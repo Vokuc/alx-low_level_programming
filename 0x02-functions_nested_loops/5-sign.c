363b4ec24964e06a74379032ea2804fa747694dc
Vokuc <ukpaival@gmail.com>
2022-04-12 15:33:39 -0700

    sign language

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * @n: int to work with
+ *
+ * Return: Exit int function  with int value
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar("+");
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar("0");
+	}
+	else
+	{
+		return (-1);
+		_putchar("-");
+	}
+}
