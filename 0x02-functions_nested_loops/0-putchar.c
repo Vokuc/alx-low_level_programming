8f231b2175581338d1684b75251ade4a8bff7885
Vokuc <ukpaival@gmail.com>
2022-04-12 07:17:44 -0700

    print putchar using _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,9 +11,14 @@
  */
 int main(void)
 {
-	char n[] = "_putchar";
-
-	_putchar(n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
