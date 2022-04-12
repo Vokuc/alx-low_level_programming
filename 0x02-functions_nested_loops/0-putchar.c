1b3995f6571f2b0f2fd6841116cc4ee6cf922c02
Vokuc <ukpaival@gmail.com>
2022-04-12 05:57:23 -0700

    print _putchar using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints _putchar
+ * followed by a new line
+ *
+ * Return :int should return an int
+ */
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+
+	return (0);
+}
