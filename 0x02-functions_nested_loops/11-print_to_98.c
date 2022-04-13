8022e5f2630c733c37571751b31b18b4ac837b87
Vokuc <ukpaival@gmail.com>
2022-04-13 07:13:00 -0700

    print out numbers from n to 98 fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,18 +14,19 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, \n", i);
+			printf("%d, ", i);
 		}
 	}
 	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, \n", i);
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
-		printf("%d, \n", norm);
+		printf("%d", norm);
 	}
+	_putchar('\n');
 }
