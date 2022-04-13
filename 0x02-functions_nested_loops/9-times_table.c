510a394fed90150f78fd90256fed4c7bc4b9d06f
Vokuc <ukpaival@gmail.com>
2022-04-13 06:11:07 -0700

    the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,10 +17,10 @@ for (mult = 0; mult <= 9; mult++)
 _putchar(',');
 _putchar(' ');
 
-prod = num * prod;
+prod = num * mult;
 
 if (prod <= 9)
-_putchar('_');
+_putchar(' ');
 else
 _putchar((prod / 10) + '0');
 
