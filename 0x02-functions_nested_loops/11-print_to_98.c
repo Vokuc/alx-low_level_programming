49eb644466d70bbddcf9e34e7525f4dd2e18d932
Vokuc <ukpaival@gmail.com>
2022-04-13 07:23:52 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,19 +14,32 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
 		}
 	}
 	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
 		}
 	}
 	else
 	{
 		printf("%d", norm);
 	}
-	_putchar('\n');
 }
