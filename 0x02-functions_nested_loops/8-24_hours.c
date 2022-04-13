80bcdb4df5e061e54406bdd24d0b5ea042615454
Vokuc <ukpaival@gmail.com>
2022-04-13 05:28:58 -0700

    print time with putchar

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point of fucntion that prints every
+ * minute of jack's day
+ */
+void jack_bauer(void)
+{
+	int hours, minutes;
+	
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
