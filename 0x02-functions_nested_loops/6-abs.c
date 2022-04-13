06d0d21479cbd961330e02655d92f31dcbca8063
Vokuc <ukpaival@gmail.com>
2022-04-13 03:59:35 -0700

    print the absolute value of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,9 +3,18 @@
 /**
  * _abs - Entry point
  *
+ * @n: parameter to be worked on
+ *
  * Return: int returns int
  */
 int _abs(int n)
 {
-	return _abs(n);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return(-n);
+	}
 }
