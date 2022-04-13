4eaca8ac4508863447e286640d84db3d57180fc8
Vokuc <ukpaival@gmail.com>
2022-04-13 05:59:55 -0700

    9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,16 +19,14 @@ void times_table(void)
 
 			prod = num * prod;
 
-			if (prod <= 9)
-			{
+			if (prod <= 9);
 				_putchar('_');
-			}
 			else
-			{
 				_putchar((prod / 10) + '0');
+
 				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
 		}
+
+			_putchar('\n');
 	}
 }
