40e225ab224b4b08c46b9adac6f09f1ec0914a00
Vokuc <ukpaival@gmail.com>
2022-04-13 06:53:19 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98 - entry point to 98 rendevous
+ * @n: number to start from
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		_putchar(98 + '0');
+	}
+}
