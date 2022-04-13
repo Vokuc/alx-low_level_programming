915ed4152744036f8bacff725cd25192037f2e38
Vokuc <ukpaival@gmail.com>
2022-04-13 03:08:12 -0700

    signprint

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar("+");
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
 		return (0);
-		_putchar("0");
+		_putchar('0');
 	}
 	else
 	{
 		return (-1);
-		_putchar("-");
+		_putchar('-');
 	}
 }
