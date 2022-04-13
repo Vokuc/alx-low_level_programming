9f288ebbadfa95a6ebadc942fd8a392a93350a10
Vokuc <ukpaival@gmail.com>
2022-04-13 06:08:16 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,27 +6,26 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+int num, mult, prod;
 
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar('0');
+for (num = 0; num <= 9; num++)
+{
+_putchar('0');
 
-		for (mult = 0; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
+for (mult = 0; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
 
-			prod = num * prod;
+prod = num * prod;
 
-			if (prod <= 9)
-				_putchar('_');
-			else
-				_putchar((prod / 10) + '0');
+if (prod <= 9)
+_putchar('_');
+else
+_putchar((prod / 10) + '0');
 
-				_putchar((prod % 10) + '0');
-		}
-
-			_putchar('\n');
-	}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
 }
