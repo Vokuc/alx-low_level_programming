17f354532098257b91ec47e8bfc5848df021fce3
Vokuc <ukpaival@gmail.com>
2022-04-14 23:07:15 -0700

    task 1 is digit

--- 0x04-more_functions_nested_loops/2-isdigit.c
+++ 0x04-more_functions_nested_loops/2-isdigit.c
@@ -2,16 +2,18 @@
 
 /**
  * _isdigit - checks for a digit 0 through 9
+ *
  * @c: digit to be checked
+ *
  * Return: 1 if true, 0 if false
  */
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
 
-	return (0);
+return (0);
 }
