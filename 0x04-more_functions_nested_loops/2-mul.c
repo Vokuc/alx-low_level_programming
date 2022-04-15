afe724eb19f3ffbe44acb6c9fd41b6d8839c56fc
Vokuc <ukpaival@gmail.com>
2022-04-14 23:30:35 -0700

    fixxed trailing white space in multiple program

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * mul - Entry point of functions that multiplies two integersi
+ * mul - Entry point of functions that multiplies two integers
  *
  * @a: The first integer
  * 
@@ -12,7 +12,7 @@
 int mul(int a, int b)
 {
 	int c = 0;
-	
+
 	c = a * b;
 
 	return (c);
