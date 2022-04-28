4b7bdf82d03f7e0676a027bc167906aadae9740c
Vokuc <ukpaival@gmail.com>
2022-04-28 05:40:35 -0700

    prints string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_puts_recursion - prints a string
  *@s: pointer block of memory to fill
@@ -6,14 +7,13 @@
  */
 
 void _puts_recursion(char *s)
-
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-_putchar (*s);
-_puts_recursion(s + 1);
+	_putchar (*s);
+	_puts_recursion(s + 1);
 }
