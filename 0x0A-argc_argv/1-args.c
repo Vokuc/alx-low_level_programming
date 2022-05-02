4f7b2941412edf93138b67e7824d8045ab318972
Vokuc <ukpaival@gmail.com>
2022-05-02 10:48:57 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of function
+ * @argc: number of arguments in the cmd line
+ * @argv: array holding the value of the args passed by argc
+ * Return: return 0
+ */
+
+int main(int argc, *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
