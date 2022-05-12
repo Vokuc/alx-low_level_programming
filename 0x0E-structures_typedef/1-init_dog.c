56ec10d41fdbb117e20c9223c767f9a0b2397ccd
Vokuc <ukpaival@gmail.com>
2022-05-12 07:02:08 -0700

    initializes a var of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#ifndef DOGINIT_H
+#define DOGINIT_H
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to variable of type struct
+ * @name: pointer to variable of type char, second param
+ * @age: pointer to variable of type float
+ * @owner: pointer to variable of type char, 4th param
+ * Description: This struct is for dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d === NULL)
+		return;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
+#endif
