2ddc27ef9cd9d11488030622a6a8fe65226fb5ec
Vokuc <ukpaival@gmail.com>
2022-05-25 05:46:37 -0700

    execute function before main()

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return.
  */
 
-void hareNtort(void) __attribute__ ((constructor))
+void hareNtort(void) __attribute__((constructor))
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
