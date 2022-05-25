41dfced4413f9fc69a766c4d022af6b51feb960b
Vokuc <ukpaival@gmail.com>
2022-05-25 05:39:06 -0700

    print test in function that executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * hareNtort - function executed before main
+ * Return: no return.
+ */
+
+void hareNtort(void) __attribute__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
