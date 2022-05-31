cfc5e611d0ae2d3b66c57beb9659edca484f9282
Vokuc <ukpaival@gmail.com>
2022-05-30 23:52:27 -0700

    returns number of bits to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
  * Return: number of bits to change
  */
 
-unsigned int flip_bits(unsigned log int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i;
 	int count = 0;
