56000551bbfd810307c6e9df052c75dbdf9466b9
Vokuc <ukpaival@gmail.com>
2022-06-01 08:20:40 -0700

    copies content of file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * main - copies the contend of a file to another file
+ * @argc: number of args passed
+ * @argv: doublle pointer
+ * Return: actual no of letter is could read and print
+ */
+int main(int argc, char **argv)
+{
+	int file1, file2, n;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file from file to \n");
+		exit(97);
+	}
+	file1 = open(argv[1], 0_RDONLY);
+	if (file1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	file2 = open(argv[2], O_CREAT | O_WRONLY, 0664);
+	while ((n = read(file1, buf, 1024)) > 0)
+	{
+		if (write(file2, buf, n) != n || file2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Catn't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(file1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file1);
+		exit(100);
+	}
+	if (close(file2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file2);
+		exit(100);
+	}
+	return (0);
+}
